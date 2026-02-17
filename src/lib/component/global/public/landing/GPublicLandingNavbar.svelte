d718163e958bc2697aeff2febd9dbb0e021a6e62
Zarni Hlawn <zarnihlawn@outlook.com>
2026-02-17 11:43:46 +0630

    feature: added landing navigation bar

---
+++ src/lib/component/global/public/landing/GPublicLandingNavbar.svelte
@@ -0,0 +1,85 @@
+<script>
+	import DaisyUiButton from '$lib/component/library/daisyui/button/DaisyUiButton.svelte';
+	import DaisyUiDropdownButton from '$lib/component/library/daisyui/dropdown/button/DaisyUiDropdownButton.svelte';
+	import DaisyUiDropdownContent from '$lib/component/library/daisyui/dropdown/content/DaisyUiDropdownContent.svelte';
+	import DaisyUiDropdown from '$lib/component/library/daisyui/dropdown/DaisyUiDropdown.svelte';
+	import DaisyUiLink from '$lib/component/library/daisyui/link/DaisyUiLink.svelte';
+	import DaisyUiNavbarCenter from '$lib/component/library/daisyui/navbar/center/DaisyUiNavbarCenter.svelte';
+	import DaisyUiNavbar from '$lib/component/library/daisyui/navbar/DaisyUiNavbar.svelte';
+	import DaisyUiNavbarEnd from '$lib/component/library/daisyui/navbar/end/DaisyUiNavbarEnd.svelte';
+	import DaisyUiNavbarStart from '$lib/component/library/daisyui/navbar/start/DaisyUiNavbarStart.svelte';
+	import LucideButterfly from '$lib/component/library/lucide/LucideButterfly.svelte';
+	import LucideMenu from '$lib/component/library/lucide/LucideMenu.svelte';
+	import { WebRoutesEnum } from '$lib/model/enum/routes.enum';
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<DaisyUiNavbar>
+	<DaisyUiNavbarStart>
+		<DaisyUiDropdown className="lg:hidden">
+			<DaisyUiDropdownButton>
+				<LucideMenu />
+			</DaisyUiDropdownButton>
+			<DaisyUiDropdownContent className="text-left">
+				<DaisyUiLink
+					href={WebRoutesEnum.DOCUMENTATION}
+					className="d-btn d-btn-ghost text-left"
+				>
+					{m.documentation()}
+				</DaisyUiLink>
+				<DaisyUiLink
+					href={WebRoutesEnum.CHANGELOG}
+					className="d-btn d-btn-ghost"
+				>
+					{m.changelog()}
+				</DaisyUiLink>
+				<DaisyUiLink
+					href={WebRoutesEnum.PRICING}
+					className="d-btn d-btn-ghost"
+				>
+					{m.pricing()}
+				</DaisyUiLink>
+			</DaisyUiDropdownContent>
+		</DaisyUiDropdown>
+		<DaisyUiLink
+			className="d-btn d-btn-ghost text-xl items-center gap-2"
+			href={WebRoutesEnum.LANDING_PAGE}
+		>
+			<LucideButterfly strokeWidth={3} />
+			{m.full_drive()}
+		</DaisyUiLink>
+	</DaisyUiNavbarStart>
+
+	<DaisyUiNavbarCenter className="gap-2 hidden lg:flex">
+		<DaisyUiLink
+			href={WebRoutesEnum.DOCUMENTATION}
+			className="d-btn d-btn-ghost
+			">{m.documentation()}</DaisyUiLink
+		>
+		<DaisyUiLink
+			href={WebRoutesEnum.CHANGELOG}
+			className="d-btn d-btn-ghost
+			">{m.changelog()}</DaisyUiLink
+		>
+		<DaisyUiLink
+			href={WebRoutesEnum.PRICING}
+			className="d-btn d-btn-ghost
+			">{m.pricing()}</DaisyUiLink
+		>
+	</DaisyUiNavbarCenter>
+
+	<DaisyUiNavbarEnd className="gap-3">
+		<DaisyUiLink
+			className="d-btn d-btn-ghost"
+			href={WebRoutesEnum.LOGIN}
+		>
+			{m.log_in()}
+		</DaisyUiLink>
+		<DaisyUiLink
+			className="d-btn d-btn-primary"
+			href={WebRoutesEnum.SIGNUP}
+		>
+			{m.sign_up()}
+		</DaisyUiLink>
+	</DaisyUiNavbarEnd>
+</DaisyUiNavbar>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { WebRoutesEnum } from '$lib/model/enum/routes.enum';
+	import { LifeCycleUtil } from '$lib/util/life-cycle.util.svelte';
+	import { RouterUtil } from '$lib/util/router.util.svelte';
+
+	const routerUtil = new RouterUtil();
+	const lifeCycleUtil = new LifeCycleUtil();
+
+	lifeCycleUtil.onMount(() => {
+		routerUtil.goToRoute(WebRoutesEnum.LANDING_PAGE);
+	});
+</script>
